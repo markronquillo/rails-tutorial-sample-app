9080f3fd61237ee3b0a540ce5a997fe407cad433
Mark Ronquillo <markronquillo23@gmail.com>
2014-01-22 15:39:38 +0800

    Add some tests in static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,30 +11,48 @@ describe "StaticPages" do
 
   subject {page}
 
+  shared_examples_for "all static pages" do
+		it { should have_selector('h1', heading) }
+		it { should have_title(title) }
+  end
+
 	describe "Home Page" do 
 		before { visit root_path }
-		it { should have_content("Sample App") }
-		it { should have_title('Ruby on Rails Tutorial Sample App') }
+		let(:heading) { 'Sample App' }
+		let(:title) { '' }
+
+		it_should_behave_like "all static pages"
 		it { should_not have_title('| Home') }
 	end
 
 	describe "Help Page" do 
 		before { visit help_path }
-		it { should have_content('Help') }
-		it { should have_title('Help') }
+		let(:heading) { 'Help' }
+		let(:title) { '' }
+
+		it_should_behave_like "all static pages"
 	end
 
 	describe "About Us Page" do
 		before { visit about_path }
-		it { should have_content('About Us') }
-		it { should have_title('About') }
+		let(:heading) { 'About' }
+		let(:title) { 'About' }
+
+		it_should_behave_like "all static pages"
 	end
 
 	describe "Contact Page" do 
 		before{ visit contact_path }
 
-		it { should have_content('Contact') }
-		it { should have_title('Contact') }
+		let(:heading) { 'Contact' }
+		let(:title) { 'Contact' }
+
+		it_should_behave_like "all static pages"
 	end
 
+	it "should have right links" do 
+		visit root_path
+		click_link "About"
+		expect(page).to have_title('About')
+	end
 end
